f8bf0b192eca5b34c2ec67d71566652eb68125c4
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-11-05 13:48:34 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
   }
 
 
-  //Your code here:
+  componentDidMount() {
+    this.handleAddTimer()
+  }
 
 
 
--- src/Timer.js
+++ src/Timer.js
@@ -6,7 +6,14 @@ class Timer extends Component {
     color: "#" + Math.floor(Math.random() * 16777215).toString(16)
   };
 
-  // add your code here
+
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 
   render() {
     const { time, color } = this.state;
